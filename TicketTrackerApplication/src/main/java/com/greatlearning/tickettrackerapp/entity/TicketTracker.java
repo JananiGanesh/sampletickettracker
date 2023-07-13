298ee38dd23ace32867e36032f1380ad47c2e74d
Jananilakshmi Gokula <g.jananilakshmimca@gmail.com>
2023-07-13 15:08:53 +0530

    Add files via upload

---
+++ TicketTrackerApplication/src/main/java/com/greatlearning/tickettrackerapp/entity/TicketTracker.java
@@ -0,0 +1,69 @@
+package com.greatlearning.tickettrackerapp.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ticket_tracker_db")
+public class TicketTracker {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ticket_id")
+	int ticketId;
+	@Column(name = "ticket_title")
+	String ticketTitle;
+	@Column(name = "ticket_short_description")
+	String ticketShortDescription;
+	@Column(name = "ticket_created_on")
+	Date ticketCreatedOn;
+	@Column(name = "ticket_content")
+	String ticketContent;
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public String getTicketTitle() {
+		return ticketTitle;
+	}
+
+	public void setTicketTitle(String ticketTitle) {
+		this.ticketTitle = ticketTitle;
+	}
+
+	public String getTicketShortDescription() {
+		return ticketShortDescription;
+	}
+
+	public void setTicketShortDescription(String ticketShortDescription) {
+		this.ticketShortDescription = ticketShortDescription;
+	}
+
+	public Date getTicketCreatedOn() {
+		return ticketCreatedOn;
+	}
+
+	public void setTicketCreatedOn(Date ticketCreatedOn) {
+		this.ticketCreatedOn = ticketCreatedOn;
+	}
+
+	public String getTicketContent() {
+		return ticketContent;
+	}
+
+	public void setTicketContent(String ticketContent) {
+		this.ticketContent = ticketContent;
+	}
+
+}
