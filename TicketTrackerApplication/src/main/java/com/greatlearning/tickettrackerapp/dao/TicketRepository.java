9d88cbd2605c8e680e12e2185ad354fb3d3bc7ac
Jananilakshmi Gokula <g.jananilakshmimca@gmail.com>
2023-07-16 08:59:40 +0530

    Delete TicketTrackerApplication directory

--- TicketTrackerApplication/src/main/java/com/greatlearning/tickettrackerapp/dao/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.tickettrackerapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.tickettrackerapp.entity.TicketTracker;
-
-public interface TicketRepository  extends JpaRepository<TicketTracker, Integer>{
-
-}
